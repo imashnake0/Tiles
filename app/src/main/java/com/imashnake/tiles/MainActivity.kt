737a92f9d5cdb37653b0d38f76cc7789ee1cfd5c
imashnake0 <kamaleshpaluru07@gmail.com>
2022-09-17 05:28:58 -0400

    Funi gradient

--- app/src/main/java/com/imashnake/tiles/MainActivity.kt
+++ app/src/main/java/com/imashnake/tiles/MainActivity.kt
@@ -3,14 +3,69 @@ package com.imashnake.tiles
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.colorspace.ColorSpaces
+import androidx.compose.ui.unit.dp
 import com.imashnake.tiles.ui.theme.TilesTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
             TilesTheme {
-                
+                val wallpaperColors = listOf(
+                    MaterialTheme.colorScheme.background,
+                    MaterialTheme.colorScheme.onPrimary,
+                    MaterialTheme.colorScheme.primaryContainer,
+                    MaterialTheme.colorScheme.onPrimaryContainer,
+                    MaterialTheme.colorScheme.primary,
+                    MaterialTheme.colorScheme.inversePrimary,
+                    MaterialTheme.colorScheme.secondary,
+                    MaterialTheme.colorScheme.onSecondary,
+                    MaterialTheme.colorScheme.secondaryContainer,
+                    MaterialTheme.colorScheme.onSecondaryContainer,
+                    MaterialTheme.colorScheme.tertiary,
+                    MaterialTheme.colorScheme.onTertiary,
+                    MaterialTheme.colorScheme.tertiaryContainer,
+                    MaterialTheme.colorScheme.onTertiaryContainer,
+                    MaterialTheme.colorScheme.onBackground,
+                    MaterialTheme.colorScheme.surface,
+                    MaterialTheme.colorScheme.onSurface,
+                    MaterialTheme.colorScheme.surfaceVariant,
+                    MaterialTheme.colorScheme.onSurfaceVariant,
+                    MaterialTheme.colorScheme.surfaceTint,
+                    MaterialTheme.colorScheme.inverseSurface,
+                    MaterialTheme.colorScheme.inverseOnSurface
+                )
+
+                val orderedColors = wallpaperColors.sortedBy { it.value }
+
+                val something = orderedColors.map { color ->
+                    List(7) { index ->
+                        color.copy(alpha = ((index.toFloat() + 1f)/7f))
+                    }
+                }.flatten()
+
+                LazyVerticalGrid(
+                    columns = GridCells.Fixed(7)
+                ) {
+                    items(something) { color ->
+                        Box(
+                            modifier = Modifier
+                                .background(color)
+                                .height(20.dp)
+                                .width(20.dp)
+                        ) {  }
+                    }
+                }
             }
         }
     }
