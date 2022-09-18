ba2b06ae64a2aa64636136f3e52ff511011017d0
imashnake0 <kamaleshpaluru07@gmail.com>
2022-09-18 00:30:35 -0400

    Downgraded `foundation` to stable
    Trying to fix the animation part 1.

--- app/src/main/java/com/imashnake/tiles/MainActivity.kt
+++ app/src/main/java/com/imashnake/tiles/MainActivity.kt
@@ -5,7 +5,6 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
@@ -17,7 +16,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.unit.dp
 import com.imashnake.tiles.ui.theme.TilesTheme
